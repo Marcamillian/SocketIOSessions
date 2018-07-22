df50494899b103c6249baecbb4f888de6ed2a9ea
Marcamillian <marcamillian@gmail.com>
2018-07-22 12:06:42 +0100

    feat: Identify returning session with an initial post request
    Returning visitors are identified via express-session using initial
     login post request and a subsiquent websocket connection opening

--- src/client/script.js
+++ src/client/script.js
@@ -1,2 +1,14 @@
-console.log("Something is happening");
+fetch('/login', {method:'POST', credentials:'same-origin'})
+.then(()=>{
+    let ws = io();
+
+    ws.on('first_visit', ()=>{
+        console.log("First time coming to this page")
+    })
+
+    ws.on('return_visit', ()=>{
+        console.log("Returning to the place")
+    })
+})
+.catch((err)=>{console.log(err.message)})
 
--- src/server/app.js
+++ src/server/app.js
@@ -1,6 +1,7 @@
 `use strict`;
 
 const session = require('express-session');
+const sharedSession = require('express-socket.io-session');
 const express = require('express');
 const http = require('http');
 const io = require('socket.io');
@@ -11,11 +12,12 @@ var sessionParser;
 var server;
 var wss;
 
+
 // create session handler
 sessionParser = session({
-    saveUninitialized: false,
     secret: 'passOpen',
-    resave: false
+    resave: true,
+    saveUninitialized: true,
 })
 
 // create express app
@@ -25,14 +27,37 @@ app.set('port', (process.env.PORT || 8080) );
 app.use(express.static('./src/client'));
 app.use(sessionParser);
 
+app.post('/login', (req, res)=>{
+    res.send({result:"OK", message: 'session set up'})
+})
+
 // create the http server
 server = http.createServer(app);
 
 // create websocket server
 wss = io(server);
+// add the session middleware to the websocket server
+wss.use(sharedSession(sessionParser, {autoSave:true}));
+
+
 // configure websocket server
 wss.on('connection', (ws)=>{
+    console.log("connection requested")
 
+    if(!ws.handshake.session.userId){ // if not aready had a session
+        ws.handshake.session.userId = "someId"
+        ws.handshake.session.save();
+        ws.emit("first_visit");
+        console.log("first time visitor")
+    }else{ // use already has a session
+        console.log(ws.handshake.session.userId)
+        ws.emit("return_visit");
+        console.log("returning visitor")
+    }
+    
+    ws.on('something', (socket)=>{
+        console.log("Some socket is doing something")
+    })
 })
 
 
